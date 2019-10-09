cce3dbe6d9b50be47567cadb4ed307bfdbcd5984
ankita jain <ankita.jain12342@gmail.com>
2019-10-09 19:16:57 -0400

    Sets

--- DAy2Swift/main.swift
+++ DAy2Swift/main.swift
@@ -33,7 +33,8 @@ a.append(101)
 a.append(50)
 a.append(60)
 a.append(80)
-print("Tootal elements in array a \(a.count)")
+a=a+[101,102,103]
+print("Total elements in array a \(a.count)")
 for i in a{
     
 print(i)
@@ -45,5 +46,53 @@ var b=Array.init(repeating: 0, count: 5)
 b[0]=11
 print(b[0])
 
-a=a+b
+//a=a+b
+
+//two dimmentional array
+var a1=[[Int]]()
+a1.append([1,2,3])
+a1.append([4,5,6])
+a1.append([7,8,9])
+print(a1[0][0])//printing particular index
+
+for row in a1{
+    print("")
+    for item in row{
+        print(item, separator: "", terminator: "")}
+}
+
+//print(a1[0][0])
+
+//a1[]=[[1,2,3],[4,5,6],[7,8,9]]
+
+//Working with Sets
+var cname=Set<String>()
+var ckey=Set<String>()
+ckey.insert("IND")
+ckey.insert("CA")
+ckey.insert("US")
+cname.insert("India")
+cname.insert("Canada")
+cname.insert("USA")
+var c=cname.union(ckey)
+print("country names")
+for j in cname.sorted(){
+    
+    print((j))
+}
+print("country codes")
+for k in ckey{
+    print(k)
+}
+print("Combined Sets")
+for i in c
+{
+print(i)
+}
+
+//Working with Dictionary
+
+
+
+
 
