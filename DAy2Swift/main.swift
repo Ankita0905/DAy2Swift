aa0f4c2c104fe76b81e7f8f7d18b272fa5845e1b
ankita jain <ankita.jain12342@gmail.com>
2019-10-09 18:39:40 -0400

    created array

--- DAy2Swift/main.swift
+++ DAy2Swift/main.swift
@@ -8,5 +8,42 @@
 
 import Foundation
 
+/*var str="aaabbcccdeeeab"
 
+for character in str{
+    for i in 0..<size{
+        print(str.index(after: i))
+    }
+   // print(character)
+}*/
+
+var a=[Int]()
+//print(a[0])
+//a[0]=100 //error
+
+
+//add new array element
+a.append(10)
+a.append(20)
+a.append(30)
+a.append(140)
+a.append(105)
+a.append(106)
+a.append(101)
+a.append(50)
+a.append(60)
+a.append(80)
+print("Tootal elements in array a \(a.count)")
+for i in a{
+    
+print(i)
+    
+}
+
+var b=Array.init(repeating: 0, count: 5)
+
+b[0]=11
+print(b[0])
+
+a=a+b
 
